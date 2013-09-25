58ad31e1f6e41a6b0544387a9f3e76c53a30797e
Scott Walters <srwalte2@illinois.edu>
2013-09-24 20:42:35 -0500

    Extended sketch functionality, finished simpleOSC example

--- simpleOSCexample.py
+++ simpleOSCexample.py
@@ -7,14 +7,13 @@ port = 9002
 initOSCClient(ip, port)
 
 sendOSCMsg("/vol", [0.05])
-sendOSCMsg("/freq", [440])
-sendOSCMsg("/bang", [1])
+sendOSCMsg("/bypass", [1])
 
 c_arp = [261.63, 329.63, 392.00, 523.25]
 
 i=0
 while(1):
-  i = (i+1)%4
   sendOSCMsg("/freq", [c_arp[i]])
   sendOSCMsg("/bang", [1])
-  time.sleep(1)
+  i = (i+1)%4
+  time.sleep(0.5)
